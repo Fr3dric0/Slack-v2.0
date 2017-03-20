a5b44c1d66f9059345cb6e08da75562d082354f4
august <august@pulpfiction>
2017-03-20 12:33:47 +0100

    fiks av mappestruktur

---
+++ client/MessageReceiver.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+from threading import Thread
+import json
+from Logger import Logger
+
+class MessageReceiver(Thread):
+    """
+    This is the message receiver class. The class inherits Thread, something that
+    is necessary to make the MessageReceiver start a new thread, and it allows
+    the chat client to both send and receive messages at the same time
+    """
+
+    def __init__(self, client, connection):
+        """
+        This method is executed when creating a new MessageReceiver object
+        """
+        Thread.__init__(self)
+        # Flag to run thread as a deamon
+        self.daemon = True
+        # TODO: Finish initialization of MessageReceiver
+        self.client = client
+        self.connection = connection
+        
+
+
+    def run(self):
+        # TODO: Make MessageReceiver receive and handle payloads        
+        try:
+            self.client.receive_message(self.connection.recv(4096).decode())
+        except Exception as e:
+            self.client.logger.error({
+                'title': 'Response Error', 
+                'message': e
+            })
+
