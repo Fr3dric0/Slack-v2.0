81d7562533452d1c6ffe389f55e31339ebcc35d6
Fredrik F Lindhagen <fred.lindh96@gmail.com>
2017-03-22 12:47:18 +0100

    Implement history model

--- server/models/history.py
+++ server/models/history.py
@@ -1,14 +1,42 @@
+import time
 
 class History:
-
+    """
+    Model for the history data. 
+    Has the ability to find
+    """
     def __init__(self, filename):
         self.filename = filename
 
     
     def find(self):
-        pass
+        f = self._read_file()
+        try:
+            return json.loads(f)
+        except:
+            return None
 
+
+    def append(self, sender, message, response = 'message'):
+        elem = self.render_mesage(sender, message, response)
+        
+        try:
+            data = json.loads(self._read_file())
+        except:
+            raise IOError('[History model] Could not convert data in {} to dict'.format(self.filename))
+
+        data.append(elem)
+        self._write_file(json.dumps(data))
+        return data
     
+
+    def render_mesage(self, sender, message, response):
+        return {
+            'timestamp': time.time(),
+            'sender': sender,
+            'response': response,
+            'content': message
+        }
 
     def _read_file(self):
         try:
@@ -18,4 +46,11 @@ class History:
             return str([])
 
     
-    
+    def _write_file(self, data):
+        try:
+            with open(filename, 'r+') as f:
+                f.write(data)
+        except:
+            return False
+        
+        return True
